62d3e9cb390d4c0a6534872475ed2a857382af70
guantino <guantino@student.42malaga.com>
2026-03-26 12:33:04 +0100

    a

--- module3/ex1/ft_score_analytics.py
+++ module3/ex1/ft_score_analytics.py
@@ -9,18 +9,26 @@ def main():
             "<score1> <score2> ..."
             )
         return
-    try:
-        scores = [int(x) for x in argv[1:]]
-        print(f"Scores processed: {scores}")
-        argc = len(scores)
-        print(f"Total players: {argc}")
-        print(f"Total score: {sum(int(x) for x in scores)}")
-        print(f"Average score: {sum(int(x) for x in scores) / argc}")
-        print(f"Low score: {int(min(scores))}")
-        print(f"High score: {int(max(scores))}")
-        print(f"Score range: {int(max(scores)) - int(min(scores))}")
-    except ValueError:
-        print("Error: all values need to be a number.")
+    scores = []
+    for i in argv[1:]:
+        try:
+            scores.append(int(i))
+        except ValueError:
+            print(f"Invalid parameter: {i}")
+    if (len(scores) == 0):
+        print(
+            "No scores provided. Usage: python3 ft_score_analytics.py "
+            "<score1> <score2> ..."
+            )
+        return
+    print(f"Scores processed: {scores}")
+    argc = len(scores)
+    print(f"Total players: {argc}")
+    print(f"Total score: {sum(int(x) for x in scores)}")
+    print(f"Average score: {sum(int(x) for x in scores) / argc}")
+    print(f"Low score: {int(min(scores))}")
+    print(f"High score: {int(max(scores))}")
+    print(f"Score range: {int(max(scores)) - int(min(scores))}")
 
 
 if __name__ == "__main__":
